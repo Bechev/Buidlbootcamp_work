908bd1bc9d9fae960a45cfed658c30a10def71b8
BCT <bertrand.chevalier@gmail.com>
2019-01-17 02:51:20 +0000

    Expand server to take an argument to identify needed task

--- experiments/my_ping_pong.py
+++ experiments/my_ping_pong.py
@@ -1,4 +1,4 @@
-import socketserver, socket
+import socketserver, socket, sys
 
 host = "0.0.0.0"
 port = 3006
@@ -11,15 +11,28 @@ class TCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         message = self.request.recv(10).strip()
+        print(message)
         print(f"got a message: {message}")
-        if message ==b"ping":
+
+        if message == b"ping":
             self.request.sendall(b"pong\n")
 
 def serve():
     server = MyTCPServer(address, TCPHandler)
     server.serve_forever()
 
-
+def ping():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(address)
+    sock.sendall(b"ping")
+    data = sock.recv(10)
+    print(f"Received {data.decode()}")
 
 if __name__ == "__main__":
-    serve()
+    command = sys.argv[1]
+    if command == "serve":
+        serve()
+    elif command == "ping":
+        ping()
+    else:
+        print("invalid command")
